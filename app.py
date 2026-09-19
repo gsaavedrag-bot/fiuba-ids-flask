57740feb70c75d620b5341e5f3dd8741b6615480
Gabriel_Alexander <gsaavedrag@fi.uba.ar>
2026-09-19 17:31:08 -0300

    Agregando puerto seguro

--- app.py
+++ app.py
@@ -13,4 +13,8 @@ app.register_blueprint(socios_bp, url_prefix='/socios')
 app.register_blueprint(reservas_bp, url_prefix='/reservas')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.getenv('FLASK_PORT', 5000))
+    if os.getenv('ENV') == 'dev':
+        app.run(debug=True, port=port)
+    else:
+        app.run(debug=False, host='0.0.0.0', port=port)
